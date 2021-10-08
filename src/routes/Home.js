da761207e754f038ca48927dff6ddccecc62d91a
Jcanotorr06 <jcano062001@gmail.com>
2021-10-08 18:31:09 -0500

    Fixed email widget width issue

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,14 @@
 import {Widget, Inbox} from '../components'
+import { Box } from '@mui/material';
 
 const Home = () => {
     return (
-        <main>
+        <Box width="100%" component="main">
             <div style={{backgroundColor: "pink", width: "100%"}}>
                 <Widget/>
             </div>
             <Inbox/>
-        </main>
+        </Box>
     )
 }
 
